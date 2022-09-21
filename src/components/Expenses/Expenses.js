8e11d2d56cb8b330dbf981eb4bb57e6ca4adffa6
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-09-21 17:43:47 +0100

    Assignment 2 - Filter year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
   const expenses = props.items;
+  const [selectedYear, setSelectedYear] = useState('2022');
+  const selectYearHandler = (filteredYear) => {
+    setSelectedYear(filteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter selected={selectedYear} onSelectYearFilter={selectYearHandler} />
+      <Card className="expenses">
+        {expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
