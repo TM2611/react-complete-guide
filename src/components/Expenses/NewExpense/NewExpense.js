5befb0ef613b85c2bc161222e8a9a1914587ad84
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-09-21 16:17:05 +0100

    comments

--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -2,6 +2,7 @@ import ExpenseForm from "./ExpenseForm"
 import "./NewExpense.css"
 
 const NewExpense = (props) => {
+    //Receives expense data from ExpenseForm, assigns the expense an ID and passes expense to parent (App)
     const submitExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
