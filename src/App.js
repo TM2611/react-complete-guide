edf2ad7950b0664172bc42c1bc80ed1508caa770
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-09-22 15:34:14 +0100

    Assignment 3 - Working with Lists

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ const DUMMY_EXPENSES = [
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2019, 5, 12),
   },
 ];
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,19 +4,21 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
-  const expenses = props.items;
   const [selectedYear, setSelectedYear] = useState('2022'); //useState returns state and a function for updating state
+
   const selectYearHandler = (filteredYear) => {
     setSelectedYear(filteredYear);
-    // console.log(filteredYear); //shows old year value, as setSelectedYear *schedules* new yearto be changed
+    // console.log(filteredYear); //would not show year value updated by prev line, as setSelectedYear *schedules* new year to be changed. 
+    // During subsequent re-renders, the first value returned by useState will always be the most recent state after applying updates.
   };
+  const filteredExpenses = props.items.filter(x => x.date.getFullYear() === Number(selectedYear))
 
 
   return (
     <div>
       <ExpensesFilter selected={selectedYear} onSelectYearFilter={selectYearHandler} />
       <Card className="expenses">
-        {expenses.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id} // Helps React identify which instances of a component have changed
             title={expense.title}
