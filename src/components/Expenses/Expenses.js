0717e49f8abb475423e49644c68371df2b9cdbc6
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-09-29 17:21:05 +0100

    67. Outputting Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,28 +4,38 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState('2022'); //useState returns state and a function for updating state
+  const [selectedYear, setSelectedYear] = useState("2022"); //useState returns state and a function for updating state
 
   const selectYearHandler = (filteredYear) => {
     setSelectedYear(filteredYear);
-    // console.log(filteredYear); //would not show year value updated by prev line, as setSelectedYear *schedules* new year to be changed. 
+    // console.log(filteredYear); //would not show year value updated by prev line, as setSelectedYear *schedules* new year to be changed.
     // During subsequent re-renders, the first value returned by useState will always be the most recent state after applying updates.
   };
-  const filteredExpenses = props.items.filter(x => x.date.getFullYear() === Number(selectedYear))
+  const filteredExpenses = props.items.filter(
+    (x) => x.date.getFullYear() === Number(selectedYear)
+  );
 
+  let expensesContent = <p>No Expenses for this year</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id} // Helps React identify which instances of a component have changed
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={selectedYear} onSelectYearFilter={selectYearHandler} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id} // Helps React identify which instances of a component have changed
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesFilter
+          selected={selectedYear}
+          onSelectYearFilter={selectYearHandler}
+        />
+        {expensesContent}
       </Card>
     </div>
   );
