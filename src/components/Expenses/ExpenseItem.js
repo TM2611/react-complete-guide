f61fb45300f0af44f740f2bb9d25da695bb72a02
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-09-30 11:25:00 +0100

    li semantics

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,18 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
   return (
-    <Card className="expense-item">       
-        <ExpenseDate date={props.date}/>  {/*props.x is an OBJECT with just the 'x' property  */}
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{`£${props.amount}`}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />{" "}
+        {/*props.x is an OBJECT with just the 'x' property  */}
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{`£${props.amount}`}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
