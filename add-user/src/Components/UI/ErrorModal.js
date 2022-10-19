4ce76a9a1e7e7bc1c9ac910a6a88714fe7ba6b74
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-10-19 17:39:09 +0100

    98. Managing the Error State

--- add-user/src/Components/UI/ErrorModal.js
+++ add-user/src/Components/UI/ErrorModal.js
@@ -5,16 +5,16 @@ import styles from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop} />
+      <div className={styles.backdrop} onClick={props.onConfirm}/>
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
         </header>
         <div className={styles.content}>
-          <p>{props.message}</p>
+          <p>{props.content}</p>
         </div>
         <footer className={styles.actions}>
-          <Button>OK</Button>
+          <Button onClick={props.onConfirm}>OK</Button>
         </footer>
       </Card>
     </div>
--- add-user/src/Components/Users/AddUser.js
+++ add-user/src/Components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
@@ -8,6 +8,7 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const usernameChangeHandler = (e) => {
     setEnteredUsername(e.target.value);
@@ -20,28 +21,54 @@ const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      return;  
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("")
-    setEnteredAge("")
+    setEnteredUsername("");
+    setEnteredAge("");
   };
+
+  const errorConfirmHandler = () => {
+    setError(null);
+  }
+
   return (
     <div>
-    <ErrorModal title="An error occurred!" content="Something went wrong!"></ErrorModal>
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler}/>
-        <label htmlFor="age">Age (years)</label>
-        <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}/>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      {error && (
+        <ErrorModal title={error.title} content={error.message} onConfirm={errorConfirmHandler}></ErrorModal>
+      )}
+
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (years)</label>
+          <input
+            type="number"
+            id="age"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
     </div>
   );
 };
