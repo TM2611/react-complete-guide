918b6a7bdc76c45d90df338d2879ce0b242e1979
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-07 13:20:09 +0000

    140. Adding a Modal via a React Portal

--- react-meals-project/src/App.js
+++ react-meals-project/src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Header";
 import Meals from "./Components/Meals/Meals";
 
 const App = () => {
   return (
-    <div>
+    <Fragment>
+      <Cart></Cart>
       <Header />
       <Meals />
-    </div>
+    </Fragment>
   );
 };
 
--- react-meals-project/src/Components/Cart/Cart.js
+++ react-meals-project/src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -7,22 +8,22 @@ const Cart = (props) => {
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
-      ;
     </ul>
   );
 
   return (
-  <div>
-    {cartItems}
-    <div className={styles.total}>
+    <Modal>
+      {cartItems}
+      <div className={styles.total}>
         <span>Total Amount</span>
         <span>35.62</span>
-    </div>
-    <div className={styles.actions}>
+      </div>
+      <div className={styles.actions}>
         <button className={styles["button--alt"]}>Close</button>
         <button className={styles.button}>Order</button>
-    </div>
-  </div>);
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
