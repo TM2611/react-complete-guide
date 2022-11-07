f3aa1978e01440e6fc879cc8f2ba688a79dc94d7
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-07 16:00:54 +0000

    142. Adding a Cart Context

--- react-meals-project/src/App.js
+++ react-meals-project/src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, {useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Header";
 import Meals from "./Components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [isCartDisplay, setIsCartDisplay] = useState(false);
@@ -15,11 +16,11 @@ const App = () => {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {isCartDisplay && <Cart onCloseCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <Meals />
-    </Fragment>
+    </CartProvider>
   );
 };
 
---
+++ react-meals-project/src/store/CartProvider.js
@@ -0,0 +1,28 @@
+// Manage cart context data and provide to other components
+import React from 'react'
+import CartContext from './cart-context'
+
+const CartProvider = (props) => {
+
+    const addItemToCartHandler = (item) =>{
+
+    }
+
+    const removeItemFromCartHandler = (id) =>{
+
+    }
+
+    const CartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    }
+  return (
+    <CartContext.Provider value={CartContext}>
+        {props.children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
