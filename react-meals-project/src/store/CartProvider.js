e50fb4db398bb60634a08032c0cf399dbec5145f
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-07 16:32:41 +0000

    144. Adding a Cart Reducer

--- react-meals-project/src/store/CartProvider.js
+++ react-meals-project/src/store/CartProvider.js
@@ -1,28 +1,45 @@
 // Manage cart context data and provide to other components
-import React from 'react'
-import CartCtx from './cart-context'
+import React, { useReducer } from "react";
+import CartCtx from "./cart-context";
+
+const initialState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "ADD_ITEM":
+      const updatedItems = state.items.concat(payload.item);
+      const updatedTotalAmount =
+        state.totalAmount + payload.item.price * payload.item.amount;
+      return { items: updatedItems, totalAmount: updatedTotalAmount };
+
+    default:
+      return state;
+  }
+};
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer);
 
-    const addItemToCartHandler = (item) =>{
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", payload: item });
+  };
 
-    }
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", payload: id });
+  };
 
-    const removeItemFromCartHandler = (id) =>{
-
-    }
-
-    const CartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    }
+  const CartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
   return (
-    <CartCtx.Provider value={CartContext}>
-        {props.children}
-    </CartCtx.Provider>
-  )
-}
+    <CartCtx.Provider value={CartContext}>{props.children}</CartCtx.Provider>
+  );
+};
 
-export default CartProvider
+export default CartProvider;
