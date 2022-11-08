b4f74828d105def0c8ddb4735ec3e04626e21fab
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-08 15:54:19 +0000

    149. Using the useEffect Hook

--- react-meals-project/src/Components/HeaderCartButton.js
+++ react-meals-project/src/Components/HeaderCartButton.js
@@ -1,17 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "./Meals/CartIcon";
 import CartCtx from "../store/cart-context";
+import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartContext = useContext(CartCtx);
+  const { items } = cartContext; 
 
-  const numberOfCartItems = cartContext.items.reduce((currValue, item) => {
+  const [isBtnHighlight, setIsBtnHighlight] = useState(false);
+
+  const numberOfCartItems = items.reduce((currValue, item) => {
     return currValue + item.amount;
   }, 0);
+
+  const btnStyles = `${styles.button} ${isBtnHighlight ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+        return;
+        
+    }
+    setIsBtnHighlight(true);
+
+    const timer = setTimeout(() => {
+        setIsBtnHighlight(false);
+    }, 300)
+
+    return () => {
+        clearTimeout(timer)
+    };
+  }, [items]);
+
   return (
     <div>
-      <button className={styles.button} onClick={props.onClick}>
+      <button className={btnStyles} onClick={props.onClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
