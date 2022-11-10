7190d54c79d76a9a99e78930999374b6a22e844f
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-10 15:07:14 +0000

    182. POST setup

--- http-project/src/App.js
+++ http-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-
+import AddMovie from './components/AddMovie';
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -8,11 +8,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async ()=> {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-http-20141-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!!");
       }
@@ -35,7 +35,11 @@ function App() {
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, [fetchMoviesHandler])
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
 
   const getDynamicContent = () => {
@@ -53,6 +57,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
