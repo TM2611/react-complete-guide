fb41036dcfd939d54e69890cbeae59781cf1c532
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-10 15:21:15 +0000

    183. Sending a POST Request

--- http-project/src/App.js
+++ http-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import AddMovie from './components/AddMovie';
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -12,21 +12,26 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://react-http-20141-default-rtdb.firebaseio.com/movies.json");
+      const response = await fetch(
+        "https://react-http-20141-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!!");
       }
+
       const data = await response.json();
 
-      const formattedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseData: movieData.release_date,
-        };
-      });
-      setMovies(formattedMovies);
+      const fetchedMovies = [];
+
+      for (const key in data) {
+        fetchedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(fetchedMovies);
     } catch (e) {
       setError(e.message);
     }
@@ -37,10 +42,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-20141-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
-
 
   const getDynamicContent = () => {
     if (isLoading) {
