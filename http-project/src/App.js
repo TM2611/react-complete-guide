3534df15c82abbbf2a56740cce58a92d6cb65ce0
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-10 13:05:20 +0000

    178. Using async / await

--- http-project/src/App.js
+++ http-project/src/App.js
@@ -6,22 +6,18 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const formattedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(formattedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const formattedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_date,
+      };
+    });
+    setMovies(formattedMovies);
   }
 
   return (
