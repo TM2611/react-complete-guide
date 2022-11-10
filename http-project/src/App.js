e95ee162add744c32fca293115419de992207451
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-10 14:18:41 +0000

    181. Using useEffect() For Requests

--- http-project/src/App.js
+++ http-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async ()=> {
     setIsLoading(true);
     setError(null);
     try {
@@ -31,7 +31,12 @@ function App() {
       setError(e.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+
 
   const getDynamicContent = () => {
     if (isLoading) {
