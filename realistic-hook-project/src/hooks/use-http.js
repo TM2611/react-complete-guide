6cdc5c3121ef223e84dac6c296e0f7cae85d3f44
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-11-11 11:57:11 +0000

    193. Using the Custom Http Hook

--- realistic-hook-project/src/hooks/use-http.js
+++ realistic-hook-project/src/hooks/use-http.js
@@ -1,6 +1,6 @@
-import { useState } from "React";
+import { useState } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = (requestConfig, transformData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -9,9 +9,9 @@ const useHttp = (requestConfig, applyData) => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
@@ -19,15 +19,7 @@ const useHttp = (requestConfig, applyData) => {
       }
 
       const data = await response.json();
-      applyData(data);
-
-      // const loadedTasks = [];
-
-      // for (const taskKey in data) {
-      //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      // }
-
-      // setTasks(loadedTasks);
+      transformData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
