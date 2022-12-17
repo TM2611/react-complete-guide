34c273d909ab3616a86c58124a694233bdcc291f
Taylor McFarlane <taylor.mcfarlane@freshrelevance.com>
2022-12-17 16:03:21 +0000

    new hook

--- forms-project/src/components/BasicForm.js
+++ forms-project/src/components/BasicForm.js
@@ -1,22 +1,53 @@
+import { useState } from "react";
+import useInput from "../hooks/use-input-v2";
+
+const validateName = (name) => {
+  return name.trim().length === 0;
+};
+
 const BasicForm = (props) => {
+  const {
+    enteredValue: enteredFname,
+    enteredValueInvalid: enteredFnameInvalid,
+    inputInvalid: fnameInputInvalid,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetFname
+  } = useInput(validateName);
+
+  const onSubmitHandler = (e) => {
+    if (enteredFnameInvalid) {
+        return;
+    }
+    resetFname(); //enteredFname + isTouched
+
+  };
+
   return (
     <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='fname'>First Name</label>
-          <input type='text' id='fname' />
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="fname">First Name</label>
+          <input
+            type="text"
+            id="fname"
+            value={enteredFname}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+          {fnameInputInvalid && <p>Name must not be empty</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='lname'>Last Name</label>
-          <input type='text' id='lname' />
+        <div className="form-control">
+          <label htmlFor="lname">Last Name</label>
+          <input type="text" id="lname" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='email'>E-Mail Address</label>
-        <input type='text' id='email' />
+      <div className="form-control">
+        <label htmlFor="email">E-Mail Address</label>
+        <input type="text" id="email" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button onSubmit={onSubmitHandler}>Submit</button>
       </div>
     </form>
   );
---
+++ forms-project/src/hooks/use-input-v2.js
@@ -0,0 +1,33 @@
+import { useState, React } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueInvalid = validateValue(enteredValue);
+  const inputInvalid = valueInvalid && isTouched;
+
+  const inputChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    enteredValue,
+    valueInvalid,
+    inputInvalid,
+    inputChangeHandler,
+    inputBlurHandler,
+    reset
+  };
+};
+
+export default useInput;
